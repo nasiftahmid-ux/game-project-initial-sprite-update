6011ebca10d34c6ad474a7ac063c3e1361134e89
tosh25xyz <toshrif1986@gmail.com>
2026-05-24 23:40:06 +0600

    Update game code

--- main.c
+++ main.c
@@ -6,7 +6,11 @@
 const int screenWidth = 800;
    
 const int screenHeight = 600;
-
+void DrawCharacter(Texture2D texture,Rectangle frameRec,Vector2 position,int frameWidth,int frameHeight);
+void charactermovement(Vector2  *position,int *currentRow);
+void clampcharacter(Vector2  *position,int frameWidth,int frameHeight);
+void animation(float *frameTime,int *currentFrame,float frameSpeed);
+void UpdateFrame(Rectangle *frameRec,int *currentFrame,int *currentRow,int frameWidth,int frameHeight);
 int main(void)
 {
     
@@ -29,64 +33,32 @@ int main(void)
 
     float frameTime = 0.0f;
     float frameSpeed = 0.15f;
-    srand(time(NULL));
+   
 
-    GenerateWorld();
+    
     
    
     while (!WindowShouldClose())
     {
         
-        if (IsKeyDown(KEY_RIGHT))
-        {
-            position.x += 2;
-            currentRow = 3;
-        }
-        else if (IsKeyDown(KEY_LEFT))
-        {
-            position.x -= 2;
-            currentRow = 2;
-        }
-        else if (IsKeyDown(KEY_UP))
-        {
-            position.y -= 2;
-            currentRow = 1;
-        }
-        else if (IsKeyDown(KEY_DOWN))
-        {
-            position.y += 2;
-            currentRow = 0;
-        }
         
+        //CHARACTER  MOVEMENT
+        charactermovement(&position,&currentRow);
         // Clamp position to screen bounds
-        position.x = Clamp(position.x, 0, screenWidth - frameWidth+50);
-        position.y = Clamp(position.y, 0, screenHeight-frameHeight+90 );   
-        
-
+        clampcharacter(&position, frameWidth,frameHeight);
         // Animation timing
-        if(IsKeyDown(KEY_UP) ||IsKeyDown(KEY_DOWN)||IsKeyDown(KEY_LEFT)||IsKeyDown(KEY_RIGHT))
-        {
-            frameTime += GetFrameTime();
-            if (frameTime >= frameSpeed)
-            {
-                frameTime = 0.0f;
-                currentFrame++;
-                if (currentFrame > 3) currentFrame = 0;
-            }
-        }
+        animation(&frameTime,&currentFrame,frameSpeed);
+        
 
         // Update frame rectangle
-        frameRec.x = currentFrame * frameWidth;
-        frameRec.y = currentRow * frameHeight;
+        UpdateFrame(&frameRec,&currentFrame,&currentRow,frameWidth,frameHeight);
+        
         
 
         BeginDrawing();
         ClearBackground(BLACK);
        
-        Rectangle destRec = { position.x, position.y, frameWidth/2.0f, frameHeight/2.0f };
-        Vector2 origin = { 0, 0 };
-
-        DrawTexturePro(texture, frameRec, destRec, origin, 0.0f, WHITE);
+        DrawCharacter(texture, frameRec,position,frameWidth,frameHeight);
         
        
         DrawFPS(10,10);
@@ -98,3 +70,60 @@ int main(void)
 
     return 0;
 }
+void DrawCharacter(Texture2D texture,Rectangle frameRec,Vector2 position,int frameWidth,int frameHeight)
+{
+    Rectangle destRec = { position.x, position.y, frameWidth/2.0f, frameHeight/2.0f };
+    Vector2 origin = {0, 0};
+    DrawTexturePro(texture, frameRec, destRec, origin, 0.0f, WHITE);    
+}
+void charactermovement(Vector2  *position,int *currentRow)
+{
+    if (IsKeyDown(KEY_RIGHT))
+        {
+            position->x += 2;
+            *currentRow = 3;
+        }
+        else if (IsKeyDown(KEY_LEFT))
+        {
+            position->x -= 2;
+            *currentRow = 2;
+        }
+        else if (IsKeyDown(KEY_UP))
+        {
+            position->y -= 2;
+            *currentRow = 1;
+        }
+        else if (IsKeyDown(KEY_DOWN))
+        {
+            position->y += 2;
+            *currentRow = 0;
+        }
+}
+void clampcharacter(Vector2  *position,int frameWidth,int frameHeight)
+{
+    position->x = Clamp(position->x, 0, screenWidth - frameWidth+50);
+    position->y = Clamp(position->y, 0, screenHeight-frameHeight+90 ); 
+}
+void animation(float *frameTime,int *currentFrame,float frameSpeed)
+{
+    if(IsKeyDown(KEY_UP) ||IsKeyDown(KEY_DOWN)||IsKeyDown(KEY_LEFT)||IsKeyDown(KEY_RIGHT))
+        {
+            *frameTime += GetFrameTime();
+            if (*frameTime >= frameSpeed)
+            {
+                *frameTime = 0.0f;
+                (*currentFrame)++;
+                if (*currentFrame > 3) 
+                {
+                    *currentFrame = 0;
+                }
+            }
+        }
+}
+void UpdateFrame(Rectangle *frameRec,int *currentFrame,int *currentRow,int frameWidth,int frameHeight)
+{
+    frameRec->x = (*currentFrame) * frameWidth;
+    frameRec->y = (*currentRow) * frameHeight;
+        
+}
+
