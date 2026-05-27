d0cf049cde69120146a6e0720dc5e01f19321b43
tosh25xyz <toshrif1986@gmail.com>
2026-05-27 16:50:57 +0600

    Full collision map set

--- collisiontest.c
+++ collisiontest.c
@@ -31,9 +31,27 @@ int IsBlocked(int x, int y)
     // if (x < 0 || y < 0 || x >= 30 || y >= 20)
     //     return 1;
 
-    if(mapoverlap[y][x] == 11 || mapoverlap[y][x] == 11)
+    if(mapoverlap[y][x] == 11 || mapoverlap[y][x] == 19 || mapoverlap[y][x] == 17|| mapoverlap[y][x] == 16)
     {
         return 1;
     }
+    else if(mapoverlap[y][x] == 13 || mapoverlap[y][x] == 29 || mapoverlap[y][x] == 30)
+    {
+        return 1;
+    }
+    else if(mapoverlap[y][x] == 25 || mapoverlap[y][x] == 28 || mapoverlap[y][x] == 26)
+    {
+        return 1;
+    }
+    else if(mapoverlap[y][x] == 23 || mapoverlap[y][x] == 24 || mapoverlap[y][x] == 22 || mapoverlap[y][x]==14 ||mapoverlap[y][x]==12)
+    {
+        return 1;
+    }
+    else if(mapoverlap[y][x] == 10 || mapoverlap[y][x] == 8 || mapoverlap[y][x] == 7 || mapoverlap[y][x]==6 ||mapoverlap[y][x]==5)
+    {
+        return 1;
+    }
+
+
     return 0;
 }
